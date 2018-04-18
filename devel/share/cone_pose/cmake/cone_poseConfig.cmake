e94dfe8475c05c543ce754b49dcb36a714024e09
diggerdata <robotincad@gmail.com>
2018-04-18 19:48:36 -0400

    Update submodules

--- devel/share/cone_pose/cmake/cone_poseConfig.cmake
+++ devel/share/cone_pose/cmake/cone_poseConfig.cmake
@@ -67,7 +67,7 @@ set(cone_pose_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
-  set(cone_pose_SOURCE_PREFIX /home/diggerdata/catkin_wpirm/src/cone_pose)
+  set(cone_pose_SOURCE_PREFIX /home/diggerdata/catkin_wpirm/src/wpirm_navigation)
   set(cone_pose_DEVEL_PREFIX /home/diggerdata/catkin_wpirm/devel)
   set(cone_pose_INSTALL_PREFIX "")
   set(cone_pose_PREFIX ${cone_pose_DEVEL_PREFIX})
@@ -110,7 +110,7 @@ if(NOT " " STREQUAL " ")
         message(FATAL_ERROR "Project 'cone_pose' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'cone_pose' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/diggerdata/catkin_wpirm/src/cone_pose/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'cone_pose' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/diggerdata/catkin_wpirm/src/wpirm_navigation/${idir}'.  ${_report}")
     endif()
     _list_append_unique(cone_pose_INCLUDE_DIRS ${include})
   endforeach()
@@ -129,7 +129,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/diggerdata/catkin_wpirm/devel/lib;/home/diggerdata/catkin_wpirm/devel/lib;/home/diggerdata/ws/gym-gazebo/gym_gazebo/envs/installation/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/diggerdata/catkin_wpirm/devel/lib;/home/diggerdata/catkin_wpirm/devel/lib;/home/diggerdata/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
